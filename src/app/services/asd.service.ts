c85ccdf9aa008bc7e8605f16cd1db4a3d8bb94db
Gustavo <gustavo.sanmartin@timestamp.cl>
2020-07-10 13:07:10 -0400

    agregando nueva clase para el sonar

---
+++ src/app/services/asd.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+  equipo: any[] = [];
+  variable:string;
+
+  constructor(private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json').subscribe( ( resp: InfoPagina )  => {
+
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+  private cargarEquipo() {
+    this.http.get('https://firstappangular-76775.firebaseio.com/equipo.json').subscribe( ( resp: any[] )  => {
+
+      this.equipo = resp;
+    });
+  }
+
+  private newMethod(){
+      
+  }
+}
